16b0e5750881306001489ba989cd5607154ee374
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-07-23 20:48:10 -0300

    feat: validate dto data.

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Higor-Edgar/booktime.git/internal/contract"
 	"github.com/Higor-Edgar/booktime.git/internal/domain"
 	"github.com/Higor-Edgar/booktime.git/internal/repository"
+	"github.com/Higor-Edgar/booktime.git/util"
 	"github.com/google/uuid"
 )
 
@@ -16,6 +17,10 @@ type userService struct {
 }
 
 func (u *userService) CreateUser(newUserDTO *contract.NewUserDTO) error {
+	if err := util.ValidateStruct(newUserDTO); err != nil {
+		return err
+	}
+
 	user := &domain.User{
 		ID:       uuid.New(),
 		Name:     newUserDTO.Name,
